3ba1a12f45e900ef13c1c1e4fc6fbafb1e55f3b8
angelkibui <a.kibui@alustudent.com>
2025-02-27 15:27:24 -0800

    fixed errors

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,40 +1,25 @@
 #!/usr/bin/python3
-import sys
+from sys import argv, exit
 from calculator_1 import add, sub, mul, div
-
-
-def calculate(a, operator, b):
-    """Perform the calculation based on the operator."""
-    if operator == '+':
-        return add(a, b)
-    elif operator == '-':
-        return sub(a, b)
-    elif operator == '*':
-        return mul(a, b)
-    elif operator == '/':
-        return div(a, b)
-    else:
-        return None
-
 
 if __name__ == "__main__":
     # Check if the number of arguments is exactly 3
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
-    # Extract arguments
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
+    a = int(argv[1])  # First number
+    operator = argv[2]  # Operator
+    b = int(argv[3])  # Second number
 
-    # Perform the calculation
-    result = calculate(a, operator, b)
+    # Dictionary to map operators to functions
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
 
-    # Handle invalid operators
-    if result is None:
+    # Check if the operator is valid
+    if operator not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
 
-    # Print the result
-    print("{} {} {} = {}".format(a, operator, b, result))
+    # Perform the operation and print the result
+    result = operators[operator](a, b)
+    print(f"{a} {operator} {b} = {result}")
